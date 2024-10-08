8128ea58b04091c0bb3c79aedaa8da463ee82277
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-08 07:47:50 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-hw-08.1@1e18ea8b84aeada7a43f726447dfa0209e8eff56 🚀

---
+++ 03-feedback.5c2291ea.js
@@ -0,0 +1,2 @@
+console.log(function(n,o){const t={};for(let n=1;n<=o;n+=1)t[n]=0;return n.forEach((n=>{const o=Object.values(t),c=Math.min(...o),e=o.indexOf(c)+1;t[e]+=n})),t}([12,3,5,6,2,15,7],2));
+//# sourceMappingURL=03-feedback.5c2291ea.js.map
